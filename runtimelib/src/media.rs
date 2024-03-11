d1d4ce87e0a782d31448986afea47a56f33f007b
Kyle Kelley <rgbkrk@gmail.com>
2024-03-11 13:02:31 -0700

    more docs, more mimetypes

--- runtimelib/src/media.rs
+++ runtimelib/src/media.rs
@@ -2,73 +2,91 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 
+/// An enumeration representing various MIME (Multipurpose Internet Mail Extensions) types.
+/// These types are used to indicate the nature of the data in a rich content message.
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum MimeType {
-    /* Plaintext media types */
+    /// Plain text, typically representing unformatted text. (e.g. Python's `_repr_` or `_repr_pretty_` methods).
     #[serde(rename = "text/plain")]
     Plain,
+    /// HTML, (as displayed via Python's `_repr_html_` method).
     #[serde(rename = "text/html")]
     Html,
+    /// LaTeX, (as displayed using Python's `_repr_latex_` method).
     #[serde(rename = "text/latex")]
     Latex,
+    /// Raw JavaScript code.
     #[serde(rename = "application/javascript")]
     Javascript,
+    /// Markdown text, (as displayed using Python's `_repr_markdown_` method).
+    #[serde(rename = "text/markdown")]
+    Markdown,
 
-    /* Text based Images */
+    /// SVG image text, (as displayed using Python's `_repr_svg_` method).
     #[serde(rename = "image/svg+xml")]
     Svg,
 
-    /* Binary Images */
+    /// PNG image data.
     #[serde(rename = "image/png")]
     Png,
+    /// JPEG image data.
     #[serde(rename = "image/jpeg")]
     Jpeg,
+    /// GIF image data.
     #[serde(rename = "image/gif")]
     Gif,
 
-    /* Pure JSON for viewing */
+    /// Raw JSON Object
     #[serde(rename = "application/json")]
     Json,
 
-    /* Special JSON Media Types that require custom renderers */
-    /* GeoJSON */
+    /// GeoJSON data, a format for encoding a variety of geographic data structures.
     #[serde(rename = "application/geo+json")]
     GeoJson,
-    /* Data Table, e.g. `{data: [], schema: {}}` */
+    /// Data table in JSON format, requires both a `data` and `schema`.
+    /// Example: `{data: [{'ghost': true, 'says': "boo"}], schema: {fields: [{name: 'ghost', type: 'boolean'}, {name: 'says', type: 'string'}]}}`.
     #[serde(rename = "application/vnd.dataresource+json")]
     DataTable,
-    /* Plotly */
+    /// Plotly JSON Schema for for rendering graphs and charts.
     #[serde(rename = "application/vnd.plotly.v1+json")]
     Plotly,
-    /* Jupyter/IPython widgets */
+    /// Jupyter/IPython widget view in JSON format.
     #[serde(rename = "application/vnd.jupyter.widget-view+json")]
     WidgetView,
+    /// Jupyter/IPython widget state in JSON format.
     #[serde(rename = "application/vnd.jupyter.widget-state+json")]
     WidgetState,
-    /* Vega & VegaLite */
+    /// VegaLite data in JSON format for version 2 visualizations.
     #[serde(rename = "application/vnd.vegalite.v2+json")]
     VegaLite2,
+    /// VegaLite data in JSON format for version 3 visualizations.
     #[serde(rename = "application/vnd.vegalite.v3+json")]
     VegaLiteV3,
+    /// VegaLite data in JSON format for version 4 visualizations.
     #[serde(rename = "application/vnd.vegalite.v4+json")]
     VegaLiteV4,
+    /// VegaLite data in JSON format for version 5 visualizations.
     #[serde(rename = "application/vnd.vegalite.v5+json")]
     VegaLiteV5,
+    /// VegaLite data in JSON format for version 6 visualizations.
     #[serde(rename = "application/vnd.vegalite.v6+json")]
     VegaLiteV6,
+    /// Vega data in JSON format for version 3 visualizations.
     #[serde(rename = "application/vnd.vega.v3+json")]
     VegaV3,
+    /// Vega data in JSON format for version 4 visualizations.
     #[serde(rename = "application/vnd.vega.v4+json")]
     VegaV4,
+    /// Vega data in JSON format for version 5 visualizations.
     #[serde(rename = "application/vnd.vega.v5+json")]
     VegaV5,
 
-    /* Virtual DOM (nteract/vdom) */
+    /// Represents Virtual DOM (nteract/vdom) data in JSON format.
     #[serde(rename = "application/vdom.v1+json")]
     Vdom,
 
-    /* Anything goes */
+    /// Represents any other MIME type not listed above.
     #[serde(other)]
     Other,
 }
@@ -80,6 +98,7 @@ impl From<String> for MimeType {
             "text/html" => MimeType::Html,
             "text/latex" => MimeType::Latex,
             "application/javascript" => MimeType::Javascript,
+            "text/markdown" => MimeType::Markdown,
 
             "application/json" => MimeType::Json,
             "application/vnd.dataresource+json" => MimeType::DataTable,
@@ -111,14 +130,28 @@ impl From<String> for MimeType {
     }
 }
 
+/// A `MimeBundle` is a collection of data associated with different MIME types.
+/// It allows for the representation of rich content that can be displayed in multiple formats.
+/// These are found in the `data` field of a `DisplayData` and `ExecuteResult` messages/output types.
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct MimeBundle {
+    /// A map of MIME types to their corresponding data, represented as JSON `Value`.
     #[serde(flatten)]
     pub content: HashMap<MimeType, Value>,
 }
 
 impl MimeBundle {
-    /* Find the first matching mime_type that is both in the priority order and this bundle. */
+    /// Find the richest media based on a priority order of MIME types.
+    /// The richest content is the first MIME type in the priority order that exists in the bundle.
+    ///
+    /// # Arguments
+    ///
+    /// * `priority_order` - A slice of `MimeType` representing the desired priority order.
+    ///
+    /// # Returns
+    ///
+    /// An `Option` containing a tuple of the selected `MimeType` and its corresponding content as a `Value`.
+    /// Returns `None` if none of the MIME types in the priority order are present in the bundle.
     pub fn richest(&self, priority_order: &[MimeType]) -> Option<(MimeType, Value)> {
         for mime_type in priority_order {
             if let Some(content) = self.content.get(mime_type) {
