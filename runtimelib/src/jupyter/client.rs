678189dee6bc72eafde722dc17f9ad67087d3085
Kyle Kelley <rgbkrk@gmail.com>
2024-03-13 11:51:02 -0700

    switch to return as naked statement

--- runtimelib/src/jupyter/client.rs
+++ runtimelib/src/jupyter/client.rs
@@ -167,7 +167,7 @@ impl JupyterClient {
 
     pub async fn next_io(&mut self) -> Result<JupyterMessage, Error> {
         let message = JupyterMessage::read(&mut self.iopub).await;
-        return message;
+        message
     }
 
     pub async fn listen(mut self) {
