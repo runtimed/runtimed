3c6677481b2f532662d7905e08ab5e91b31abe3f
Kyle Kelley <rgbkrk@gmail.com>
2024-05-27 20:24:34 -0700

    capture media type values on enum

--- runtimelib/src/media.rs
+++ runtimelib/src/media.rs
@@ -1,133 +1,130 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+type JsonObject = serde_json::Map<String, serde_json::Value>;
 
 /// An enumeration representing various MIME (Multipurpose Internet Mail Extensions) types.
 /// These types are used to indicate the nature of the data in a rich content message.
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash, Clone)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 #[serde(rename_all = "snake_case")]
+#[serde(tag = "type", content = "data")]
 pub enum MimeType {
     /// Plain text, typically representing unformatted text. (e.g. Python's `_repr_` or `_repr_pretty_` methods).
     #[serde(rename = "text/plain")]
-    Plain,
+    Plain(String),
     /// HTML, (as displayed via Python's `_repr_html_` method).
     #[serde(rename = "text/html")]
-    Html,
+    Html(String),
     /// LaTeX, (as displayed using Python's `_repr_latex_` method).
     #[serde(rename = "text/latex")]
-    Latex,
+    Latex(String),
     /// Raw JavaScript code.
     #[serde(rename = "application/javascript")]
-    Javascript,
+    Javascript(String),
     /// Markdown text, (as displayed using Python's `_repr_markdown_` method).
     #[serde(rename = "text/markdown")]
-    Markdown,
+    Markdown(String),
 
     /// SVG image text, (as displayed using Python's `_repr_svg_` method).
     #[serde(rename = "image/svg+xml")]
-    Svg,
+    Svg(String),
 
     /// PNG image data.
     #[serde(rename = "image/png")]
-    Png,
+    Png(Vec<u8>),
     /// JPEG image data.
     #[serde(rename = "image/jpeg")]
-    Jpeg,
+    Jpeg(Vec<u8>),
     /// GIF image data.
     #[serde(rename = "image/gif")]
-    Gif,
+    Gif(Vec<u8>),
 
     /// Raw JSON Object
     #[serde(rename = "application/json")]
-    Json,
+    Json(JsonObject),
 
     /// GeoJSON data, a format for encoding a variety of geographic data structures.
     #[serde(rename = "application/geo+json")]
-    GeoJson,
+    GeoJson(JsonObject),
     /// Data table in JSON format, requires both a `data` and `schema`.
     /// Example: `{data: [{'ghost': true, 'says': "boo"}], schema: {fields: [{name: 'ghost', type: 'boolean'}, {name: 'says', type: 'string'}]}}`.
     #[serde(rename = "application/vnd.dataresource+json")]
-    DataTable,
+    DataTable(JsonObject),
     /// Plotly JSON Schema for for rendering graphs and charts.
     #[serde(rename = "application/vnd.plotly.v1+json")]
-    Plotly,
+    Plotly(JsonObject),
     /// Jupyter/IPython widget view in JSON format.
     #[serde(rename = "application/vnd.jupyter.widget-view+json")]
-    WidgetView,
+    WidgetView(JsonObject),
     /// Jupyter/IPython widget state in JSON format.
     #[serde(rename = "application/vnd.jupyter.widget-state+json")]
-    WidgetState,
+    WidgetState(JsonObject),
     /// VegaLite data in JSON format for version 2 visualizations.
     #[serde(rename = "application/vnd.vegalite.v2+json")]
-    VegaLiteV2,
+    VegaLiteV2(JsonObject),
     /// VegaLite data in JSON format for version 3 visualizations.
     #[serde(rename = "application/vnd.vegalite.v3+json")]
-    VegaLiteV3,
+    VegaLiteV3(JsonObject),
     /// VegaLite data in JSON format for version 4 visualizations.
     #[serde(rename = "application/vnd.vegalite.v4+json")]
-    VegaLiteV4,
+    VegaLiteV4(JsonObject),
     /// VegaLite data in JSON format for version 5 visualizations.
     #[serde(rename = "application/vnd.vegalite.v5+json")]
-    VegaLiteV5,
+    VegaLiteV5(JsonObject),
     /// VegaLite data in JSON format for version 6 visualizations.
     #[serde(rename = "application/vnd.vegalite.v6+json")]
-    VegaLiteV6,
+    VegaLiteV6(JsonObject),
     /// Vega data in JSON format for version 3 visualizations.
     #[serde(rename = "application/vnd.vega.v3+json")]
-    VegaV3,
+    VegaV3(JsonObject),
     /// Vega data in JSON format for version 4 visualizations.
     #[serde(rename = "application/vnd.vega.v4+json")]
-    VegaV4,
+    VegaV4(JsonObject),
     /// Vega data in JSON format for version 5 visualizations.
     #[serde(rename = "application/vnd.vega.v5+json")]
-    VegaV5,
+    VegaV5(JsonObject),
 
     /// Represents Virtual DOM (nteract/vdom) data in JSON format.
     #[serde(rename = "application/vdom.v1+json")]
-    Vdom,
+    Vdom(JsonObject),
 
     // Catch all type for serde ease.
     // TODO: Implement a custom deserializer so this extra type isn't in resulting serializations.
-    #[serde(other, rename = "application/vnd.runtimelib.unknown")]
-    Other,
+    Other((String, Value)),
 }
 
-impl From<String> for MimeType {
-    fn from(s: String) -> Self {
-        match s.as_str() {
-            "text/plain" => MimeType::Plain,
-            "text/html" => MimeType::Html,
-            "text/latex" => MimeType::Latex,
-            "application/javascript" => MimeType::Javascript,
-            "text/markdown" => MimeType::Markdown,
+impl std::hash::Hash for MimeType {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let mime_type_str = match &self {
+            MimeType::Plain(_) => "text/plain",
+            MimeType::Html(_) => "text/html",
+            MimeType::Latex(_) => "text/latex",
+            MimeType::Javascript(_) => "application/javascript",
+            MimeType::Markdown(_) => "text/markdown",
+            MimeType::Svg(_) => "image/svg+xml",
+            MimeType::Png(_) => "image/png",
+            MimeType::Jpeg(_) => "image/jpeg",
+            MimeType::Gif(_) => "image/gif",
+            MimeType::Json(_) => "application/json",
+            MimeType::GeoJson(_) => "application/geo+json",
+            MimeType::DataTable(_) => "application/vnd.dataresource+json",
+            MimeType::Plotly(_) => "application/vnd.plotly.v1+json",
+            MimeType::WidgetView(_) => "application/vnd.jupyter.widget-view+json",
+            MimeType::WidgetState(_) => "application/vnd.jupyter.widget-state+json",
+            MimeType::VegaLiteV2(_) => "application/vnd.vegalite.v2+json",
+            MimeType::VegaLiteV3(_) => "application/vnd.vegalite.v3+json",
+            MimeType::VegaLiteV4(_) => "application/vnd.vegalite.v4+json",
+            MimeType::VegaLiteV5(_) => "application/vnd.vegalite.v5+json",
+            MimeType::VegaLiteV6(_) => "application/vnd.vegalite.v6+json",
+            MimeType::VegaV3(_) => "application/vnd.vega.v3+json",
+            MimeType::VegaV4(_) => "application/vnd.vega.v4+json",
+            MimeType::VegaV5(_) => "application/vnd.vega.v5+json",
+            MimeType::Vdom(_) => "application/vdom.v1+json",
+            MimeType::Other((key, _)) => key.as_str(),
+        };
 
-            "application/json" => MimeType::Json,
-            "application/vnd.dataresource+json" => MimeType::DataTable,
-            "application/vnd.plotly.v1+json" => MimeType::Plotly,
-            "image/svg+xml" => MimeType::Svg,
-            "image/png" => MimeType::Png,
-            "image/jpeg" => MimeType::Jpeg,
-            "image/gif" => MimeType::Gif,
-
-            "application/vnd.jupyter.widget-view+json" => MimeType::WidgetView,
-            "application/vnd.jupyter.widget-state+json" => MimeType::WidgetState,
-
-            "application/geo+json" => MimeType::GeoJson,
-
-            "application/vnd.vegalite.v2+json" => MimeType::VegaLiteV2,
-            "application/vnd.vegalite.v3+json" => MimeType::VegaLiteV3,
-            "application/vnd.vegalite.v4+json" => MimeType::VegaLiteV4,
-            "application/vnd.vegalite.v5+json" => MimeType::VegaLiteV5,
-            "application/vnd.vegalite.v6+json" => MimeType::VegaLiteV6,
-
-            "application/vnd.vega.v3+json" => MimeType::VegaV3,
-            "application/vnd.vega.v4+json" => MimeType::VegaV4,
-            "application/vnd.vega.v5+json" => MimeType::VegaV5,
-
-            "application/vdom.v1+json" => MimeType::Vdom,
-
-            _ => MimeType::Other,
-        }
+        mime_type_str.hash(state);
     }
 }
 
@@ -135,61 +132,57 @@ impl From<String> for MimeType {
 /// It allows for the representation of rich content that can be displayed in multiple formats.
 /// These are found in the `data` field of a `DisplayData` and `ExecuteResult` messages/output types.
 ///
-/// # Examples
-///
-/// ```rust
-/// use runtimelib::media::{MimeBundle, MimeType};
-///
-/// let raw = r#"{
-///    "text/plain": "FancyThing()",
-///    "text/html": "<h1>Fancy!</h1>",
-///    "application/json": {"fancy": true}
-/// }"#;
-///
-/// let mime_bundle: MimeBundle = serde_json::from_str(raw).unwrap();
-///
-/// let richest = mime_bundle.richest(&[MimeType::Html, MimeType::Json, MimeType::Plain]);
-///
-/// if let Some((mime_type, content)) = richest {
-///    println!("Richest MIME type: {:?}", mime_type);
-///    println!("Content: {:?}", content);
-/// }
-/// ```
-///
-#[derive(Default, Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[derive(Default, Serialize, Deserialize, Debug, Clone)]
 pub struct MimeBundle {
     /// A map of MIME types to their corresponding data, represented as JSON `Value`.
-    #[serde(flatten)]
-    pub content: HashMap<MimeType, Value>,
+    #[serde(flatten, deserialize_with = "deserialize_mimebundle")]
+    pub content: HashSet<MimeType>,
+}
 
-    #[serde(flatten)]
-    pub unknown_content: HashMap<String, Value>,
+fn deserialize_mimebundle<'de, D>(deserializer: D) -> Result<HashSet<MimeType>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let map: HashMap<String, Value> = HashMap::deserialize(deserializer)?;
+    let mut set = HashSet::new();
+
+    for (key, value) in map {
+        let mut mime_value_map = serde_json::Map::new();
+        mime_value_map.insert("type".to_string(), Value::String(key.clone()));
+        mime_value_map.insert("data".to_string(), value.clone());
+
+        let mime_type: MimeType = match serde_json::from_value(Value::Object(mime_value_map)) {
+            Ok(mime_type) => mime_type,
+            Err(_) => MimeType::Other((key, value)),
+        };
+
+        set.insert(mime_type);
+    }
+
+    Ok(set)
 }
 
 impl MimeBundle {
-    /// Find the richest media based on a priority order of MIME types.
-    /// The richest content is the first MIME type in the priority order that exists in the bundle.
-    ///
-    /// # Arguments
-    ///
-    /// * `priority_order` - A slice of `MimeType` representing the desired priority order.
-    ///
-    /// # Returns
-    ///
-    /// An `Option` containing a tuple of the selected `MimeType` and its corresponding content as a `Value`.
-    /// Returns `None` if none of the MIME types in the priority order are present in the bundle.
-    pub fn richest(&self, priority_order: &[MimeType]) -> Option<(MimeType, Value)> {
-        for mime_type in priority_order {
-            if let Some(content) = self.content.get(mime_type) {
-                return Some((mime_type.clone(), content.clone()));
-            }
-        }
-        None
+    pub fn richest(&self, ranker: fn(&MimeType) -> usize) -> Option<&MimeType> {
+        self.content
+            .iter()
+            .filter_map(|mimetype| {
+                let rank = ranker(mimetype);
+                if rank > 0 {
+                    Some((rank, mimetype))
+                } else {
+                    None
+                }
+            })
+            .max_by_key(|(rank, _)| *rank)
+            .map(|(_, mimetype)| mimetype)
     }
 }
 
 #[cfg(test)]
 mod test {
+    use serde_json::json;
+
     use super::*;
 
     #[test]
@@ -218,12 +211,16 @@ mod test {
 
         let bundle: MimeBundle = serde_json::from_str(raw).unwrap();
 
-        let richest = bundle.richest(&[MimeType::Html]);
+        let ranker = |mime_type: &MimeType| match mime_type {
+            MimeType::Plain(_) => 1,
+            MimeType::Html(_) => 2,
+            _ => 0,
+        };
 
-        let (mime_type, content) = richest.unwrap();
-
-        assert_eq!(mime_type, MimeType::Html);
-        assert_eq!(content, "<h1>Hello, world!</h1>");
+        match bundle.richest(ranker) {
+            Some(MimeType::Html(data)) => assert_eq!(data, "<h1>Hello, world!</h1>"),
+            _ => panic!("Unexpected mime type"),
+        }
     }
 
     #[test]
@@ -252,26 +249,28 @@ mod test {
 
         let bundle: MimeBundle = serde_json::from_str(raw).unwrap();
 
-        let richest = bundle.richest(&[MimeType::DataTable, MimeType::Json, MimeType::Html]);
+        let ranker = |mime_type: &MimeType| match mime_type {
+            MimeType::Html(_) => 1,
+            MimeType::Json(_) => 2,
+            MimeType::DataTable(_) => 3,
+            _ => 0,
+        };
 
-        let (mime_type, content) = richest.unwrap();
+        let richest = bundle.richest(ranker);
 
-        assert_eq!(mime_type, MimeType::DataTable);
-        assert_eq!(
-            content,
-            serde_json::json!({
-                "data": [
-                    {"name": "Alice", "age": 25},
-                    {"name": "Bob", "age": 35}
-                ],
-                "schema": {
-                    "fields": [
-                        {"name": "name", "type": "string"},
-                        {"name": "age", "type": "integer"}
-                    ]
-                }
-            })
-        );
+        match richest {
+            Some(MimeType::DataTable(data)) => {
+                assert_eq!(
+                    data["data"],
+                    json!([{"name": "Alice", "age": 25}, {"name": "Bob", "age": 35}])
+                );
+                assert_eq!(
+                    data["schema"]["fields"],
+                    json!([{"name": "name", "type": "string"}, {"name": "age", "type": "integer"}])
+                );
+            }
+            _ => panic!("Unexpected mime type"),
+        }
     }
 
     #[test]
@@ -282,27 +281,21 @@ mod test {
 
         let bundle: MimeBundle = serde_json::from_str(raw).unwrap();
 
-        let richest = bundle.richest(&[
-            MimeType::DataTable,
-            MimeType::Json,
-            MimeType::Html,
-            MimeType::Svg,
-            MimeType::Plain,
-        ]);
+        let ranker = |mime_type: &MimeType| match mime_type {
+            MimeType::Html(_) => 1,
+            MimeType::Json(_) => 2,
+            MimeType::DataTable(_) => 3,
+            _ => 0,
+        };
 
-        let binary_data = bundle.unknown_content.get("application/fancy");
-        assert_eq!(
-            binary_data,
-            Some(&serde_json::json!("Too ✨ Fancy ✨ for you!"))
-        );
+        let richest = bundle.richest(ranker);
 
         assert_eq!(richest, None);
-    }
 
-    #[test]
-    fn from_string() {
-        let mime_type: MimeType = "text/plain".to_string().into();
-        assert_eq!(mime_type, MimeType::Plain);
+        assert!(bundle.content.contains(&MimeType::Other((
+            "application/fancy".to_string(),
+            json!("Too ✨ Fancy ✨ for you!")
+        ))));
     }
 
     #[test]
@@ -330,48 +323,7 @@ mod test {
         }"#;
 
         let bundle: MimeBundle = serde_json::from_str(raw).unwrap();
-
-        let richest = bundle.richest(&[]);
+        let richest = bundle.richest(|_| 0);
         assert_eq!(richest, None);
-    }
-
-    #[test]
-    fn direct_access() {
-        let raw = r#"{
-            "text/plain": "🦀 Hello from Rust! 🦀",
-            "text/html": "<h1>Hello, world!</h1>",
-            "application/json": {
-                "name": "John Doe",
-                "age": 30
-            },
-            "application/vnd.dataresource+json": {
-                "data": [
-                    {"name": "Alice", "age": 25},
-                    {"name": "Bob", "age": 35}
-                ],
-                "schema": {
-                    "fields": [
-                        {"name": "name", "type": "string"},
-                        {"name": "age", "type": "integer"}
-                    ]
-                }
-            },
-            "application/octet-stream": "Binary data"
-        }"#;
-
-        let bundle: MimeBundle = serde_json::from_str(raw).unwrap();
-
-        assert_eq!(
-            bundle.content.get(&MimeType::Html).unwrap(),
-            &serde_json::json!("<h1>Hello, world!</h1>")
-        );
-
-        assert_eq!(
-            bundle
-                .content
-                .get(&MimeType::from("text/plain".to_string()))
-                .unwrap(),
-            "🦀 Hello from Rust! 🦀"
-        )
     }
 }
