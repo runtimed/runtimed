cf70df5552aeb9bf0e7f0f2e8c8aaadcabc606e0
Kyle Kelley <rgbkrk@gmail.com>
2024-10-28 15:26:09 -0700

    remove runtimed and runt packages

--- crates/runtimed/migrations/20240227065653_create_runtime_instances.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-CREATE TABLE runtime_instances (
-	id blob NOT NULL,
-	name text NOT NULL
-);
--- crates/runtimed/src/state.rs
+++
@@ -1,9 +0,0 @@
-use crate::runtime_manager::RuntimeManager;
-use sqlx::Pool;
-use sqlx::Sqlite;
-
-#[derive(Clone)]
-pub struct AppState {
-    pub dbpool: Pool<Sqlite>,
-    pub runtimes: RuntimeManager,
-}
