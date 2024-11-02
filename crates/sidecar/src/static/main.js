5dbca1f2c58a4e510e7bdeef9471852e67dbf1ed
Trevor Manz <trevor.j.manz@gmail.com>
2024-11-02 01:28:07 -0400

    Support ipywidgets

--- crates/sidecar/src/static/main.js
+++ crates/sidecar/src/static/main.js
@@ -1,239 +1,177 @@
-// @ts-check
-/// <reference lib="dom" />
-
-/** @type {Map<string, Model>} */
-let MODELS = new Map();
+/** @import * as t from "./types.ts" */
 
 /**
- * @param {number} n
- * @returns {HTMLDivElement}
+ * @param {number | undefined} executionCount
  */
-function createOutputCell(n) {
-  let cell = document.createElement("div");
+function createOutputCell(executionCount) {
+  const cell = document.createElement("div");
   cell.className = "cell";
-  if (n !== undefined) {
-    // @ts-expect-error this field is required in the css
-    cell.dataset.n = n;
+  if (executionCount !== undefined) {
+    cell.dataset.n = executionCount.toString();
   }
-  let outputArea = document.querySelector("#outputArea");
+  const outputArea = document.querySelector("#outputArea");
   assert(outputArea, "outputArea not found");
   outputArea.appendChild(cell);
   return cell;
 }
 
 /**
- * @param {import("./types.ts").JupyterMessage} msg
- * @returns {msg is import("./types.ts").DisplayData | import("./types.ts").ExecuteResult}
+ * @param {t.JupyterMessage} msg
+ * @returns {msg is t.DisplayData | t.ExecuteResult}
  */
 function isDisplayDataOrExecuteResult(msg) {
   return msg.header.msg_type === "display_data" ||
     msg.header.msg_type === "execute_result";
 }
 
-/**
- * @param {import("./types.ts").JupyterMessage} msg
- */
-export function onMessage(msg) {
-  if (isDisplayDataOrExecuteResult(msg)) {
-    let { data, execution_count } = msg.content;
-    let output = createOutputCell(execution_count);
+/** @param {t.JupyterMessage} message */
+export async function onMessage(message) {
+  /** @type {import("npm:@jupyter-widgets/html-manager").HTMLManager} */
+  const manager = globalThis.widgetManager;
+  assert(manager, "widgetManager not found");
+
+  if (isDisplayDataOrExecuteResult(message)) {
+    const { data, execution_count } = message.content;
+    const output = createOutputCell(execution_count);
     if (data["application/vnd.jupyter.widget-view+json"]) {
-      displayWidget(data["application/vnd.jupyter.widget-view+json"], output);
+      const { model_id } = data["application/vnd.jupyter.widget-view+json"];
+      const model = await manager.get_model(model_id);
+      const view = await manager.create_view(model, {});
+      // @ts-expect-error - @jupyter-widgets/html-manager is incorrectly typed. I hate this package.
+      await manager.display_view(view, { el: output });
     } else if (data["text/html"]) {
       output.innerHTML = data["text/html"];
     } else if (data["text/plain"]) {
-      let pre = document.createElement("pre");
+      const pre = document.createElement("pre");
       pre.textContent = data["text/plain"];
       output.appendChild(pre);
     }
     return;
   }
 
-  if (msg.header.msg_type === "comm_open") {
-    let bufferPaths = msg.content.data.buffer_paths;
-    let state = msg.content.data.state;
-    let id = msg.content.comm_id;
-    if (MODELS.has(id)) {
-      return;
-    }
-    MODELS.set(id, new Model(state, { id }));
+  if (message.header.msg_type === "comm_open") {
+    const commId = message.content.comm_id;
+    const comm = new Comm(commId, message.header);
+    manager.handle_comm_open(
+      comm,
+      // I seriously don't get this API.
+      // Half of the methods are `any` and the other half are super strictly typed.
+      // @ts-expect-error - very strict and we have the right message.
+      message,
+    );
     return;
   }
 
-  if (msg.header.msg_type === "stream") {
+  if (message.header.msg_type === "comm_msg") {
+    const commId = message.content.comm_id;
+    manager.get_model(commId).then((model) => {
+      // @ts-expect-error we know our comm has this method
+      model.comm?.handle_msg(message);
+    });
+    return;
+  }
+
+  if (message.header.msg_type === "comm_close") {
+    const commId = message.content.comm_id;
+    manager.get_model(commId).then((model) => {
+      // @ts-expect-error we know our comm has this method
+      model.comm?.handle_close(message);
+    });
+    return;
+  }
+
+  if (message.header.msg_type === "stream") {
     console.log("stream");
     return;
   }
 
-  console.log(msg.header.msg_type, msg);
+  console.log(message.header.msg_type, message);
 }
 
-/**
- * @param {import("./types.ts").JupyterWidgetDisplayData} widget
- * @param {HTMLDivElement} root
- */
-async function displayWidget(widget, root) {
-  let shadowRoot = root.attachShadow({ mode: "open" });
-
-  let model = MODELS.get(widget.model_id);
-  if (!model) {
-    console.warn("Model not found", widget.model_id);
-    return;
-  }
-
-  let css = model.get("_css");
-  if (typeof css == "string") {
-    let stylesheet = new CSSStyleSheet();
-    stylesheet.replaceSync(css);
-    shadowRoot.adoptedStyleSheets = [stylesheet];
-  }
-
-  let esm = model.get("_esm");
-  assert(typeof esm === "string", "esm not found");
-
-  let el = document.createElement("div");
-  {
-    let blob = new Blob([esm], { type: "application/javascript" });
-    let url = URL.createObjectURL(blob);
-    let mod = await import(url);
-
-    if (!mod.default) return;
-    await mod.default.initialize?.({ model });
-    await mod.default.render?.({ model, el });
-  }
-
-  shadowRoot.appendChild(el);
-}
-
-class Model {
-  /** @type {Record<string, unknown>} */
-  #state = {};
-  /** @type {EventTarget} */
-  #target = new EventTarget();
+// This class is a striped down version of Comm from @jupyter-widgets/base
+export class Comm {
   /** @type {string} */
-  #id;
+  comm_id;
+  /** @type {"jupyter.widgets"} */
+  get target_name() {
+    return "jupyter.widgets";
+  }
+  /** @type {((x: any) => void) | undefined} */
+  #on_msg = undefined;
+  /** @type {((x: any) => void) | undefined} */
+  #on_close = undefined;
+
+  /** @type {Record<string, unknown>} */
+  #header;
 
   /**
-   * @param {import("./types.ts").JupyterMessage} msg
+   * @param {string} modelId
+   * @param {Record<string, unknown>} header
    */
-  async #send(msg) {
-    await fetch("/message", { method: "POST", body: JSON.stringify(msg) });
-  }
-
-  /**
-   * @param {Record<string, unknown>} initialState
-   * @param {{ id: string }} options
-   */
-  constructor(initialState, { id }) {
-    this.#state = initialState;
-    this.#id = id;
-  }
-  /**
-   * @param {string} key
-   * @returns {unknown}
-   */
-  get(key) {
-    return this.#state[key];
-  }
-  /**
-   * @param {string} key
-   * @param {unknown} value
-   */
-  set(key, value) {
-    this.#state[key] = value;
-    this.#target.dispatchEvent(new CustomEvent(`change:${key}`));
-    this.#target.dispatchEvent(new CustomEvent("change"));
-  }
-  /**
-   * @param {string} event
-   * @param {() => void} handler
-   */
-  on(event, handler) {
-    this.#target.addEventListener(event, handler);
-  }
-  /**
-   * @param {string} event
-   * @param {() => void} handler
-   */
-  off(event, handler) {
-    this.#target.removeEventListener(event, handler);
-  }
-  /**
-   * @param {unknown} msg
-   */
-  send(msg) {
-    // not implemented
-  }
-  save_changes() {}
-}
-
-class Comm {
-  target_name = "jupyter.widget";
-
-  /** @param {string} id */
-  constructor(id) {
-    /** @type {string} */
-    this.comm_id = id;
+  constructor(modelId, header) {
+    this.comm_id = modelId;
+    this.#header = header;
   }
 
   /**
-   * Opens a sibling comm in the backend
-   * @param {object} data
-   * @param {unknown=} callbacks
-   * @param {unknown=} metadata
-   * @param {ArrayBuffer[]=} buffers
-   * @return msg id
+   * @param {t.JsonValue} data
+   * @param {unknown} _callbacks
+   * @param {unknown} metadata
+   * @param {Array<ArrayBuffer>} buffers
    */
-  open(data, callbacks, metadata, buffers) {
-    console.log("open", data);
-  }
+  send(data, _callbacks, metadata, buffers) {
+    const msg = {
+      content: { comm_id: this.comm_id, data: data },
+      metadata: metadata,
+      // TODO: need to _encode_ any buffers into base64 (JSON.stringify just drops them)
+      buffers: buffers || [],
+      // this doesn't seem relevant to the widget?
+      header: {
+        ...this.#header,
+        msg_id: "TODO",
+        msg_type: "comm_msg",
+        date: new Date().toISOString(),
+      },
+    };
 
-  /**
-   * Sends a message to the sibling comm in the backend
-   * @param {object} data
-   * @param {unknown=} callbacks
-   * @param {unknown=} metadata
-   * @param {ArrayBuffer[]=} buffers
-   */
-  send(data, callbacks, metadata, buffers) {
-    console.log("send", data);
-  }
+    // await this?
+    fetch("/message", { method: "POST", body: JSON.stringify(msg) });
 
-  /**
-   * Closes the sibling comm in the backend
-   * @param {object} data
-   * @param {unknown=} callbacks
-   * @param {unknown=} metadata
-   * @param {ArrayBuffer[]=} buffers
-   */
-  close(data, callbacks, metadata, buffers) {
-    console.log("close", data);
+    return this.comm_id;
   }
-
-  /**
-   * Register a message handler
-   * @param {(msg: unknown) => void} callback, which is given a message
-   */
-  on_msg(callback) {
-    console.log("on_msg", callback);
+  open() {
+    // I don't think we need to do anything here?
+    return this.comm_id;
   }
-
-  /**
-   * Register a handler for when the comm is closed by the backend
-   * @param {(msg: unknown) => void} callback, which is given a message
-   */
-  on_close(callback) {
-    console.log("on_close", callback);
+  close() {
+    // I don't think we need to do anything here?
+    return this.comm_id;
+  }
+  /** @param {(x: any) => void} cb */
+  on_msg(cb) {
+    this.#on_msg = cb.bind(this);
+  }
+  /** @param {(x: any) => void} cb */
+  on_close(cb) {
+    this.#on_close = cb.bind(this);
+  }
+  /** @param {unknown} msg */
+  handle_msg(msg) {
+    this.#on_msg?.(msg);
+  }
+  /** @param {unknown} msg */
+  handle_close(msg) {
+    this.#on_close?.(msg);
   }
 }
 
 /**
- * @param {unknown} cond
+ * @param {unknown} condition
  * @param {string} message
- * @returns {asserts cond}
+ * @returns {asserts condition}
  */
-function assert(cond, message) {
-  if (!cond) {
+function assert(condition, message) {
+  if (!condition) {
     throw new Error(message);
   }
 }
--- crates/sidecar/src/static/types.ts
+++ crates/sidecar/src/static/types.ts
@@ -1,45 +1,49 @@
 type Header<MsgType> = {
-	msg_type: MsgType;
+  msg_type: MsgType;
 };
 
 export type JupyterWidgetDisplayData = {
-	model_id: string;
-	version_major: number;
-	version_minor: number;
+  model_id: string;
+  version_major: number;
+  version_minor: number;
 };
 
 type Mimebundle = {
-	["text/plain"]?: string;
-	["text/html"]?: string;
-	["application/vnd.jupyter.widget-view+json"]?: JupyterWidgetDisplayData;
+  ["text/plain"]?: string;
+  ["text/html"]?: string;
+  ["application/vnd.jupyter.widget-view+json"]?: JupyterWidgetDisplayData;
 };
 
 export type DisplayData = {
-	header: Header<"display_data">;
-	content: {
-		data: Mimebundle;
-		execution_count: number;
-	};
+  header: Header<"display_data">;
+  content: {
+    data: Mimebundle;
+    execution_count: number;
+  };
 };
 
 export type ExecuteResult = {
-	header: Header<"execute_result">;
-	content: {
-		data: Mimebundle;
-		execution_count: number;
-	};
+  header: Header<"execute_result">;
+  content: {
+    data: Mimebundle;
+    execution_count: number;
+  };
 };
 
 export type CommOpen = {
-	header: Header<"comm_open">;
-	content: {
-		comm_id: string;
-		target_name: "jupyter.widget";
-		data: {
-			buffer_paths: string[];
-			state: Record<string, unknown>;
-		};
-	};
+  header: Header<"comm_open">;
+  content: {
+    comm_id: string;
+    target_name: "jupyter.widget";
+    data: {
+      buffer_paths: string[];
+      state: Record<string, unknown>;
+    };
+  };
 };
 
 export type JupyterMessage = DisplayData | ExecuteResult | CommOpen;
+
+export type JsonValue = string | number | boolean | null | Array<JsonValue> | {
+  [key: string]: JsonValue;
+};
