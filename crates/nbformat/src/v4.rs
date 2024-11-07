369ee607659517ad4000ebd1b76c6e0843b6d244
Trevor Manz <trevor.j.manz@gmail.com>
2024-11-06 19:35:12 -0500

    Omit extra jupyter fields

--- crates/nbformat/src/v4.rs
+++ crates/nbformat/src/v4.rs
@@ -339,7 +339,9 @@ pub struct CellMetadata {
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct JupyterCellMetadata {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source_hidden: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub outputs_hidden: Option<bool>,
 }
 
