50504eb13bd749ed022549e49161efd8c9320a1a
Kyle Kelley <rgbkrk@gmail.com>
2024-11-13 10:19:01 -0800

    setup initial traits for jupyter protocol usage

--- crates/jupyter-protocol/src/lib.rs
+++ crates/jupyter-protocol/src/lib.rs
@@ -1,1 +1,45 @@
 pub use jupyter_serde::*;
+
+pub mod messaging;
+
+pub use messaging::*;
+
+use async_trait::async_trait;
+use std::error::Error;
+
+#[async_trait]
+pub trait JupyterConnection {
+    type Error: Error + Send + Sync + 'static;
+    type SendHalf: JupyterSender<Error = Self::Error>;
+    type ReceiveHalf: JupyterReceiver<Error = Self::Error>;
+
+    async fn send(&mut self, message: JupyterMessage) -> Result<(), Self::Error>;
+    async fn receive(&mut self) -> Result<JupyterMessage, Self::Error>;
+    fn split(self) -> (Self::SendHalf, Self::ReceiveHalf);
+}
+
+#[async_trait]
+pub trait JupyterSender {
+    type Error: Error + Send + Sync + 'static;
+
+    async fn send(&mut self, message: JupyterMessage) -> Result<(), Self::Error>;
+}
+
+#[async_trait]
+pub trait JupyterReceiver {
+    type Error: Error + Send + Sync + 'static;
+
+    async fn receive(&mut self) -> Result<JupyterMessage, Self::Error>;
+}
+
+#[async_trait]
+pub trait JupyterClient: JupyterConnection {
+    async fn connect(&mut self) -> Result<(), Self::Error>;
+    async fn disconnect(&mut self) -> Result<(), Self::Error>;
+}
+
+#[async_trait]
+pub trait JupyterKernel: JupyterConnection {
+    async fn start(&mut self) -> Result<(), Self::Error>;
+    async fn shutdown(&mut self) -> Result<(), Self::Error>;
+}
