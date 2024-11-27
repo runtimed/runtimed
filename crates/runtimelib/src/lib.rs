4fb72459e250429e9b59f451d57e296ba253ed67
Kyle Kelley <rgbkrk@gmail.com>
2024-11-27 11:38:40 -0800

    conditionally include README.md in lib.rs

--- crates/runtimelib/src/lib.rs
+++ crates/runtimelib/src/lib.rs
@@ -2,7 +2,10 @@ pub use jupyter_serde::media;
 pub use jupyter_serde::media::*;
 pub use jupyter_serde::ExecutionCount;
 
+#[cfg(feature = "tokio-runtime")]
 #[doc = include_str!("../README.md")]
+pub struct ReadmeDocumentation;
+
 pub mod jupyter;
 pub mod messaging;
 pub use jupyter::*;
